f1866197e619b669ac5f60416f146b022bbbf3db
Farhad Joya <143786101+FarhadJoya907@users.noreply.github.com>
2025-02-23 10:31:56 +0430

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,21 +1,19 @@
-import os, platform, time, pycurl
-os.system('clear')
-print('checking updates')
-os.system('git pull')
-print('first allow permations (y)')
-os.system('termux-setup-storage')
+import os, platform
+
 try:
+
     import requests
+
 except:
+
     os.system('pip install requests')
-try:
-    import pycurl
-except:
-    os.system('pip install pycurl')
-os.system('git pull')
+
 import requests
+
 bit = platform.architecture()[0]
+
 if bit == '64bit':
-    print("\n\x1b[1;92m Congratulations ! Your Device Support This Tool")
-    from FKING import menu___
-    menu___()
+
+    from FKING import main
+
+    main()
